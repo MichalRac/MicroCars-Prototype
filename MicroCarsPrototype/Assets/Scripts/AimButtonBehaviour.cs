c6ff356db82ce9fd7086d59e040cb5d6df6a982f
Michał Rać <rac.michalmail@gmail.com>
2019-03-03 19:44:02 +0100

    Creating SampleScene, Player object and basic Aim button object feature.

---
+++ MicroCarsPrototype/Assets/Scripts/AimButtonBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AimButtonBehaviour : MonoBehaviour {
+
+    public Transform aimDirectionAsset;
+    private Vector2 inputPosition;
+    public Transform car;
+    private Transform aimButton;
+    public Vector2 defaultPosition;
+    Vector2 touchPoint;
+
+    // Use this for initialization
+
+    void Start () {
+        aimButton = GetComponent<Transform>();
+	}
+	
+
+
+	// Update is called once per frame
+	public void moveAimButtonToTouchPosition () {
+
+
+        touchPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        aimButton.position = touchPoint;
+        
+	}
+
+    public void resetPosition()
+    {
+
+        aimButton.localPosition = defaultPosition;
+    }
+}
---
+++ MicroCarsPrototype/Assets/Scripts/CarPhysics.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarPhysics : MonoBehaviour {
+
+    private const float minMovingSpeed = 0.01f;
+
+    private bool isMoving = false;
+    private float carSpeed = 0;
+    private Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Move(float moveForce)
+    {
+
+        Vector2 moveForceVector = new Vector2(0.0f, moveForce);
+        rb2D.AddRelativeForce(moveForceVector);
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetButton("Jump")){
+            Move(10);
+        }
+
+    }
+}
