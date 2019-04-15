5ecb3fbd8c7227ad7127c40e5be051623c991ba6
Michał Rać <rac.michalmail@gmail.com>
2019-04-15 03:00:44 +0200

    MainMenu Scene and simple background with some cars crashing

---
+++ MicroCarsPrototype/Assets/Scripts/OutgameScripts/MenuCarBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCarBehaviour : MonoBehaviour
+{
+
+    Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.AddRelativeForce(new Vector2(0, 250));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.sqrMagnitude > 200.0f)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
