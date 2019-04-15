5a2f6b6aeed1271b0026b69f908c5fd653569dfb
Michał Rać <rac.michalmail@gmail.com>
2019-04-16 01:06:11 +0200

    Adding initial position for up to four cars

--- MicroCarsPrototype/Assets/Scripts/Ingame Scripts/GameController.cs
+++ MicroCarsPrototype/Assets/Scripts/Ingame Scripts/GameController.cs
@@ -6,13 +6,33 @@ public class GameController : MonoBehaviour
 {
 
     private bool isPlayerTurn { get; set; }  //Player turn: moment when he can Aim
+    private const int MaxPlayerNumber = 4;
+   
     
+    public GameObject[] cars = new GameObject[MaxPlayerNumber];
+    public GameObject[] startingPosition = new GameObject[MaxPlayerNumber];
+    public float[] rotation = new float[MaxPlayerNumber];
+
     public AimButtonBehaviour aimButtonBehaviour;
     public CarPhysics carPhysics;
     public UISceneControler uiSceneControler;
 
+
     void Start()
     {
+
+        for (int i = 0; i < startingPosition.Length; i++)
+        {
+            if (cars[i] == null)
+                break;
+
+
+            Quaternion carStartingRotation = Quaternion.Euler(0.0f, 0.0f, rotation[i]);
+
+            cars[i].transform.position = startingPosition[i].transform.position;
+            cars[i].transform.rotation = carStartingRotation;
+        }
+
         switchTurnState(true);
         StartAimingTurn();
     }
